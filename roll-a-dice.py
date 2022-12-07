1c927bfd9901eb4cf49afeff1b88d5f34a4de09e
Giu_ <88463562+GiuDeMelo@users.noreply.github.com>
2022-12-07 10:24:03 -0300

    Add files via upload

---
+++ roll-a-dice.py
@@ -0,0 +1,61 @@
+import numpy
+import random
+
+no = random.randint(1,6)
+
+def yn(inp):
+    #y
+    #n
+    print("teste")
+    
+
+if no == 1:
+    print("")
+    print("[-----]")
+    print("[     ]")
+    print("[  0  ]")
+    print("[     ]")
+    print("[-----]")
+    
+if no == 2:
+    print("")
+    print("[-----]")
+    print("[0    ]")
+    print("[     ]")
+    print("[    0]")
+    print("[-----]")
+
+if no == 3:
+    print("")
+    print("[-----]")
+    print("[0    ]")
+    print("[  0  ]")
+    print("[    0]")
+    print("[-----]")
+
+if no == 4:
+    print("")
+    print("[-----]")
+    print("[0   0]")
+    print("[     ]")
+    print("[0   0]")
+    print("[-----]")
+
+if no == 5:
+    print("")
+    print("[-----]")
+    print("[0   0]")
+    print("[  0  ]")
+    print("[0   0]")
+    print("[-----]")
+
+if no == 6:
+    print("")
+    print("[-----]")
+    print("[0 0 0]")
+    print("[     ]")
+    print("[0 0 0]")
+    print("[-----]")
+
+print("")
+inp = input("Pressione Y para jogar novamente e N para sair: ")
